e7d0f20d80ce35947de2fb25198b1d281aeb63b2
Chris Gates <cgates@umich.edu>
2024-05-28 16:44:00 -0400

    Update setup instructions

--- knit_site.R
+++ knit_site.R
@@ -3,7 +3,7 @@ library(rmarkdown)
 # The html from the files below don't have the nav bar
 
 # render('source/workshop_setup/preworkshop_checklist.md', output_dir='html/workshop_setup/')
-# render('source/workshop_setup/setup_instructions.md', output_dir='html/workshop_setup/')
+render('source/workshop_setup/setup_instructions.md', output_dir='html/workshop_setup/')
 # render('source/workshop_setup/setup_instructions_advanced.md', output_dir='html/workshop_setup/')
 
 # The html from the files below do have the nav bar, so if you make changes 
